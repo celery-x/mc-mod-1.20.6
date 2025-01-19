21d1beb52eff3dee6905ef484a106a1d41549ddc
superxuqc~ <751799928@qq.com>
2025-01-20 00:25:56 +0800

    分身初版

--- src/main/java/top/superxuqc/mcmod/entity/goal/playself/UseToolGoal.java
+++ src/main/java/top/superxuqc/mcmod/entity/goal/playself/UseToolGoal.java
@@ -2,6 +2,7 @@ package top.superxuqc.mcmod.entity.goal.playself;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.pattern.CachedBlockPosition;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
@@ -11,6 +12,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
 import net.minecraft.world.World;
+import top.superxuqc.mcmod.common.TargetEntity;
 import top.superxuqc.mcmod.register.ModTarKeys;
 
 import java.util.List;
@@ -18,13 +20,13 @@ import java.util.function.Function;
 
 public class UseToolGoal extends Goal {
 
-    public LivingEntity entity;
+    public TameableEntity entity;
 
     public ItemStack handItemStack;
 
     public World world;
 
-    private final EntityNavigation navigation;
+    private EntityNavigation navigation;
 
     public BlockPos pos;
 
@@ -36,32 +38,64 @@ public class UseToolGoal extends Goal {
         this.entity = entity;
         this.world = world;
         this.width = width;
-        this.pos = entity.getBlockPos();
-        this.navigation = entity.getNavigation();
+
     }
 
     @Override
     public boolean canStart() {
+        if (target != null) {
+            return true;
+        }
         if (handItemStack == null) {
             handItemStack = entity.getMainHandStack();
+        }
+        pos = entity.getBlockPos();
+        if (navigation == null) {
+            navigation = entity.getNavigation();
         }
         if (handItemStack.isIn(ModTarKeys.SCARE_SELF_ENCHANT_TAG)) {
             for (int i = -width; i <= width; i++) {
                 for (int j = -width; j < width; j++) {
                     for (int k = -width; k < width; k++) {
-                        BlockPos t = new BlockPos(pos.getX() + i, pos.getY() + j, pos.getY() + k);
-                        boolean b = handItemStack.canBreak(new CachedBlockPosition(world, t, false));
-                        List<ItemStack> droppedStacks = Block.getDroppedStacks(world.getBlockState(pos), (ServerWorld) world, pos, null, entity, handItemStack);
-                        if (droppedStacks != null && !droppedStacks.isEmpty()) {
-                            System.out.println("发现: " + t);
-                            System.out.println("内容: " + world.getBlockState(t));
-                            target = t;
-                            return true;
+                        BlockPos t = new BlockPos(pos.getX() + i, pos.getY() + j, pos.getZ() + k);
+                        if (navigation.findPathTo(t, 10) == null) {
+                            continue;
+                        }
+                        for (int l = 1; l <= 3; l++) {
+
+                            if (testAttack(t.add(0, l, 1))) return true;
+
+                            if (testAttack(t.add(0, l, -1))) return true;
+
+                            if (testAttack(t.add(1, l, 0))) return true;
+
+                            if (testAttack(t.add(-1, l, 0))) return true;
                         }
                     }
                 }
             }
         }
         return false;
+    }
+
+    private boolean testAttack(BlockPos t) {
+        if (handItemStack.getComponents().get(DataComponentTypes.TOOL).isCorrectForDrops(world.getBlockState(t))){
+            System.out.println("发现: " + t);
+            System.out.println("内容: " + world.getBlockState(t));
+            target = t;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (target != null && !entity.getPos().equals(target)) {
+            boolean b = navigation.startMovingTo(target.getX(), target.getY(), target.getZ(),5);
+        } else if (target != null) {
+
+            entity.lookAtEntity(new TargetEntity(pos.getX(), pos.getY(), pos.getZ()), 90, 90);
+        }
     }
 }
