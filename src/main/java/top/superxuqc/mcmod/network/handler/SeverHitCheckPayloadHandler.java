1b25f4b6e4e61ed7744b3da9d48e26c82c4be29c
superxuqc~ <751799928@qq.com>
2025-01-20 23:32:22 +0800

    将刀光对方块的破坏力合理化,同TNT

--- src/main/java/top/superxuqc/mcmod/network/handler/SeverHitCheckPayloadHandler.java
+++ src/main/java/top/superxuqc/mcmod/network/handler/SeverHitCheckPayloadHandler.java
@@ -1,19 +1,37 @@
 package top.superxuqc.mcmod.network.handler;
 
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.explosion.Explosion;
 import top.superxuqc.mcmod.network.payload.HitCheckPayload;
 
+import java.util.Optional;
+
 public class SeverHitCheckPayloadHandler implements ServerPlayNetworking.PlayPayloadHandler<HitCheckPayload> {
+
+    public Optional<Float> getBlastResistance(BlockState blockState, FluidState fluidState) {
+        return blockState.isAir() && fluidState.isEmpty()
+                ? Optional.empty()
+                : Optional.of(Math.max(blockState.getBlock().getBlastResistance(), fluidState.getBlastResistance()));
+    }
 
 
     @Override
     public void receive(HitCheckPayload hitCheckPayload, ServerPlayNetworking.Context context) {
         context.server().execute(() -> {
             ServerWorld world = context.server().getWorld(context.player().getSpawnPointDimension());
-            world.breakBlock(hitCheckPayload.blockPos(), false);
+            float blastResistance = world.getBlockState(hitCheckPayload.blockPos()).getBlock().getBlastResistance();
+            float h = 4;
+            h -= (blastResistance + 0.3F) * 0.3F;
+            if (h > 0.0F ) {
+                world.breakBlock(hitCheckPayload.blockPos(), false);
+            }
             if (!world.isClient()) {
                 try {
                     world.iterateEntities().forEach(v -> {
